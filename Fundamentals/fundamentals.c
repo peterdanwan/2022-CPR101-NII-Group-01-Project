8ffba3cd150bfca63b408ce61b9ce09ab634d008
Latvad <103061419+Latvad@users.noreply.github.com>
2022-04-08 23:05:03 -0400

    Fundamental Version 2 Release

--- Fundamentals/fundamentals.c
+++ Fundamentals/fundamentals.c
@@ -1,31 +1,17 @@
-/*
-Origin: Lathesan Vadivelu, lvadivelu@myseneca.ca, 075216143, April 4, 2022, CPR101, Group Project
-[FileName] : [fundamentals.c]
-Purpose: [Indexing Strings, Finds string location if it exists]
-*/
-
-
 #include "fundamentals.h"																	            // Includes: <stdio.h>, <stdlib.h>, <string.h> and the void fundamentals(); prototype.
 
 void fundamentals() {
-	
-/*
+
+	/*
 Purpose: [Indexing Strings, Finds string location if it exists]
-*/
-	
-	
-	
-	
-	
-	
-	
-	
-	/* Version 1 */
+
+
+	///* Version 1 */
 	printf("*** Start of Indexing Strings Demo ***\n");													// Displays Start of Indexing Strings Demo module to User.
 	char buffer1[80];
 	char num_input[10];
 	unsigned int position;
-	printf("Type not empty string (q - to quit):\n");													// Display: Requests user to enter a String that is not empty.
+	printf("Type not empty string (q - to quit):\n");													// Display: Requests user to empy a String that is not empty.
 	printf("01234567890123456789012345678901234567890123456789012345678901234567890123456789");			// Prints the number range [0-9] 8 Times without any other characters
 	gets(buffer1);																						// Ask user for a non empy string
 	while (strcmp(buffer1, "q") != 0) {
@@ -42,12 +28,22 @@ Purpose: [Indexing Strings, Finds string location if it exists]
 		gets(buffer1);
 	}
 
-/* Version 2 */
-//>> insert here
 
+	/* Version 2 */
+	printf("*** Start of Measuring Strings Demo ***\n");	//Notifying user that Measuring Strings Demo is starting	
+	char buffer2[80];
+	printf("Type a string (q - to quit):\n");			    //Asking user to type a string
+	gets(buffer2);
+	while (strcmp(buffer2, "q") != 0) {
+		printf("The length is %lu\n", strlen(buffer2));		//Printing the length of the string
+		printf("Type a string (q - to quit):\n");		    //Asking user to type a string
+		gets(buffer2);
+	}
+	printf("*** End of Measuring Strings Demo ***\n\n");	//Notifying user that Measuring Strings Demo is ending
+
+}
 
 /* Version 3 */
 //>> insert here
 
 
-}
